b994a93fa2de7aad91f844cdc679f26687dd5b19
althaf ahmed <157505657+Altf4ah@users.noreply.github.com>
2026-09-18 10:03:12 +0530

    Remove hardcoded Google Maps API key

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -46,7 +46,7 @@ export default function MapView({
 }) {
   const { isLoaded, loadError } = useJsApiLoader({
     id: "linecut-google-map",
-    googleMapsApiKey: apiKey || "AIzaSyCyqn2kUpF0-yb_zflx2sXoJXcL4iSwZnU",
+    googleMapsApiKey: apiKey || "",
   });
   const [map, setMap] = useState(null);
 
